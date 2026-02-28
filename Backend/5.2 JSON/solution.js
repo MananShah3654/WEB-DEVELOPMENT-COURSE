071cd4b2c29738df7ecd1b71244171de71f357d4
Manan Shah <mananshah3654@gmail.com>
2026-02-28 19:58:19 +0530

    till web api topics

---
+++ Backend/5.2 JSON/solution.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+const recipeJSON =
+   '[{"id": "0001","type": "taco","name": "Black Bean Taco","price": 2.49,"ingredients": {"protein": {"name": "Black Beans","preparation": "Seasoned"},  "salsa": {"name": "Tomato Salsa","spiciness": "Medium"},  "toppings": [{"name": "Lettuce",  "quantity": "1 cup",  "ingredients": ["Iceberg Lettuce"]  },      {"name": "Cheese",  "quantity": "1/2 cup",  "ingredients": ["Cheddar Cheese", "Monterey Jack Cheese"]  },      {"name": "Guacamole",  "quantity": "2 tablespoons",  "ingredients": ["Avocado", "Lime Juice", "Salt", "Onion", "Cilantro"]  },      {"name": "Sour Cream",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream"]  }      ]    }  },{"id": "0002","type": "taco","name": "Roasted Veggie Taco","price": 2.99,"ingredients": {"protein": {"name": "Roasted Vegetables","preparation": "Oven Roasted"},  "salsa": {"name": "Salsa Verde","spiciness": "Hot"},  "toppings": [{"name": "Onions",  "quantity": "1/4 cup",  "ingredients": ["White Onion", "Red Onion"]  },      {"name": "Cilantro",  "quantity": "2 tablespoons",  "ingredients": ["Fresh Cilantro"]  },      {"name": "Queso Fresco",  "quantity": "1/4 cup",  "ingredients": ["Queso Fresco"]  }      ]    }  },{"id": "0003","type": "taco","name": "Mushroom Taco","price": 3.49,"ingredients": {"protein": {"name": "Portobello Mushroom","preparation": "Grilled and Seasoned"},  "salsa": {"name": "Chipotle Sauce","spiciness": "Mild"},  "toppings": [{"name": "Cabbage Slaw",  "quantity": "1 cup",  "ingredients": [    "Shredded Cabbage",    "Carrot",    "Mayonnaise",    "Lime Juice",    "Salt"          ]  },      {"name": "Pico de Gallo",  "quantity": "1/2 cup",  "ingredients": ["Tomato", "Onion", "Cilantro", "Lime Juice", "Salt"]  },      {"name": "Lime Crema",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream", "Lime Juice", "Salt"]  }      ]    }  }]';
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let data;
+
+app.get("/", (req, res) => {
+  res.render("solution.ejs", { recipe: data });
+});
+
+app.post("/recipe", (req, res) => {
+  switch (req.body.choice) {
+    case "bean":
+      data = JSON.parse(recipeJSON)[0];
+      break;
+    case "veggie": 
+      data = JSON.parse(recipeJSON)[1];
+      break;
+    case "mushroom":
+      data = JSON.parse(recipeJSON)[2];
+      break;
+    default:
+      break;
+  }
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
